2f2493dc4f22f0218169650f9a62752fceac2667
rebellious_i <learneryolo3@gmail.com>
2025-07-28 14:43:17 +0500

    learned command list

---
+++ example.py
@@ -0,0 +1,4 @@
+print("Her zaman çalışırım")
+
+if __name__ == '__main__':
+    print("Sadece doğrudan çalıştırıldığımda çalışırım")
---
+++ task.py
@@ -0,0 +1,55 @@
+import sys
+import json
+import os
+from datetime import datetime
+
+TASKS_FILE='tasks.json'
+
+def load_tasks():
+    if not os.path.exists(TASKS_FILE):
+        return []
+    with open(TASKS_FILE,'r') as f:
+        return json.load(f)
+
+def  save_tasks(tasks):
+    with open(TASKS_FILE,'w') as f:
+        json.dump(tasks,f,indent=4)
+
+def add_task(description):
+    tasks=load_tasks()
+    task_id=tasks[-1]['id']+1 if tasks else 1
+    now=datetime.now().isoformat()
+    task={
+        'id':task_id,
+        'description':description,
+        'status':'todo',
+        'createdAt':now,
+        'updatedAt':now
+    }
+    tasks.append(task)
+    save_tasks(tasks)
+    print(f'Task addded succesfully (ID:{task_id})')
+    
+def list_tasks():
+    tasks=load_tasks()
+    if not tasks:
+        print('no tasks found')
+        return
+    for task  in tasks:
+        print(f"id:{task['id']},Description:{task['description']},status{task['status']},createdAt:{task['createdAt']},updatedAt:{task['updatedAt']}")
+    
+if __name__=='__main__':
+    args=sys.argv
+    if len(args)<2:
+        print('usage:python task.py commoand ]arguments"')
+    elif args[1]=='add':
+        if len(args)<3:
+              print('usage:python task.py add "your task descritpion"')  
+        else:
+           add_task(args[2])
+    elif args[1]=='list':
+        list_tasks()
+    else:
+        print(f'unknown command {args[1]}')
+        
+print(sys.argv)
