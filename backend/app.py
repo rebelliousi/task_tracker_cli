ec80bbfd4f2409ccdd29e1a6533dafa30c8662d0
rebellious_i <learneryolo3@gmail.com>
2025-07-30 15:11:31 +0500

    learned how to delete task in flask

--- backend/app.py
+++ backend/app.py
@@ -27,5 +27,21 @@ def add_task():
     tasks.append(task)
     return jsonify(task),201
 
+@app.route('/tasks/<int:task_id>',methods=['PUT'])
+def complete_task(task_id):
+ for task in tasks: 
+     if task['id']==task_id:
+         task['completed']=True
+         return jsonify(task) 
+ return jsonify({'error':'task not found'}),404  
+
+@app.route('/tasks/<int:task_id>',methods=['DELETE'])
+def delete_task(task_id):
+    global tasks
+    for task in tasks:
+        if task['id']==task_id:
+         tasks=[task for task in tasks if task['id']!=task_id]
+         return jsonify({'message ':f'Task {task_id} deleted succesfully'}),204
+    return jsonify({'error':'task not found'}),404
 if __name__=='__main__':
     app.run(debug=True)
